6fe1d6b3ebee6ad3e6ccc47275c5e3c9fbdc5d82
Lucis <luciannojunior@gmail.com>
2023-01-10 13:28:45 -0300

    Complementar exemplo

--- routes/ola.tsx
+++ routes/ola.tsx
@@ -2,50 +2,65 @@ import type { Handlers } from "$fresh/server.ts";
 
 type Opportunity = {
   source: {
-    name: string // Original.io
-    url: string // https://originalio.solides.jobs/
-  }
+    name: string; // Original.io
+    url: string; // https://originalio.solides.jobs/
+  };
   title: string; // Software Engineer
   // ...
-}
+};
+
 export const handler: Handlers = {
   GET: async (req, ctx) => {
-    const data = await fetch(
+    const originalPromise = fetch(
       "https://api.solides.jobs/v2/vacancy/search?reference_id=61196&search=&page=1&pagination=25&vacancyType=jobs"
-    ).then((r) => r.json()) as SolidesJobs;
+    ).then((r) => r.json());
 
-    return Response.json({ teste: data });
+    const outraOrgPromise = fetch(
+      "https://api.solides.jobs/v2/vacancy/search?reference_id=92&search=&page=1&pagination=25&vacancyType=jobs"
+    ).then((r) => r.json());
+
+    const [original, outra] = await Promise.all([
+      originalPromise,
+      outraOrgPromise,
+    ]);
+
+    // 1. Requisitar APIs de todas as fontes (dica: usar Promise.all ao invés de vários await fetch)
+    // 2. Identificar o tipo de retorno (Quicktype)
+    // 3. Mapear todas as vagas para um schema comum (pode se inspirar no da Solide)
+    // 4. Retornar um array de jobs
+
+    return Response.json({ teste: null });
   },
 };
 
 export interface SolidesJobs {
-  code_request_jobs:       number;
-  code_request_gestao:     number;
-  data:                    Datum[];
-  totalRecords:            number;
-  totalPages:              number;
-  page:                    number;
-  pagination:              number;
+  code_request_jobs: number;
+  code_request_gestao: number;
+  data: Datum[];
+  totalRecords: number;
+  totalPages: number;
+  page: number;
+  pagination: number;
   totalAvailablePositions: number;
 }
 
 export interface Datum {
-  id:                 number;
-  name:               string;
-  departament:        null;
-  place:              null;
-  city:               City;
-  company:            Company;
-  pcdOnly:            boolean;
-  state:              State;
-  description:        null;
-  videoUrl:           null;
-  linkVacancy:        string;
+  id: number;
+  name: string;
+  departament: null;
+  place: null;
+  city: City;
+  company: Company;
+  pcdOnly: boolean;
+  state: State;
+  description: null;
+  videoUrl: null;
+  linkVacancy: string;
   availablePositions: number;
 }
 
 export interface City {
-  id:   number;
+  id: number;
   name: string;
 }
 
@@ -55,7 +70,6 @@ export interface Company {
 }
 
 export interface State {
-  name:    string;
+  name: string;
   acronym: string;
 }
-
