8fb189844d7396395a8f3204b0c3257a57cc41ed
Hemillainy Santos <hemillainy.santos@ccc.ufcg.edu.br>
2023-02-09 22:48:01 -0300

    fix lint

--- scrapers/Solides/index.ts
+++ scrapers/Solides/index.ts
@@ -1,91 +1,93 @@
-import * as cheerio from 'https://esm.sh/cheerio@1.0.0-rc.12'
+import * as cheerio from "https://esm.sh/cheerio@1.0.0-rc.12";
 
-import { Opportunity } from '../../architecture/Opportunity.ts'
-import { Scraper } from '../../architecture/Scraper.ts'
+import { Opportunity } from "../../architecture/Opportunity.ts";
+import { Scraper } from "../../architecture/Scraper.ts";
+import { createHashId } from "../../utils/utils.ts";
 
 type SolidesResponse = {
-  code_request_jobs: number
-  totalPages: number
-  data: Vacancy[]
-}
+  code_request_jobs: number;
+  totalPages: number;
+  data: Vacancy[];
+};
 
 type Vacancy = {
-  id: number
-  name: string
+  id: number;
+  name: string;
   city: {
-    name: string
-  }
+    name: string;
+  };
   state: {
-    acronym: string
-  }
-  pcdOnly: boolean
-  linkVacancy: string
-  availablePositions: number
-  description?: string
-}
+    acronym: string;
+  };
+  pcdOnly: boolean;
+  linkVacancy: string;
+  availablePositions: number;
+  description?: string;
+};
 
 export abstract class SolidesScraper implements Scraper {
-  url = ''
+  url = "";
   source = {
-    name: '',
-    url: '',
-  }
+    name: "",
+    url: "",
+  };
 
   async execute() {
-    let opportunities: Opportunity[] = []
+    let opportunities: Opportunity[] = [];
 
     try {
       const { totalPages } = (await fetch(this.url).then((res) =>
-        res.json(),
-      )) as SolidesResponse
+        res.json()
+      )) as SolidesResponse;
 
-      const pages = Array.from({ length: totalPages }, (_, i) => i + 1)
+      const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
 
       await Promise.all(pages.map((page) => this.getVacancies(page))).then(
         (results) => {
           for (const result of results) {
-            opportunities = opportunities.concat(result)
+            opportunities = opportunities.concat(result);
           }
         },
-      )
+      );
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
 
-    return opportunities
+    return opportunities;
   }
 
   private async getVacancies(page: number) {
     try {
       const { data } = (await fetch(this.url + `&page=${page}`).then((res) =>
-        res.json(),
-      )) as SolidesResponse
+        res.json()
+      )) as SolidesResponse;
 
       return await Promise.all(
-        data.map((vacancy) => this.getOpportunity(vacancy)),
+        data.map((vacancy) =>
+          this.getOpportunity(vacancy)
+        ),
       )
         .then((opportunities) => opportunities)
-        .catch(() => [])
+        .catch(() => []);
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
 
-    return []
+    return [];
   }
 
   private async getOpportunity(vacancy: Vacancy): Promise<Opportunity> {
-    let subtitle =
-      vacancy.city.name +
-      ' / ' +
+    let subtitle = vacancy.city.name +
+      " / " +
       vacancy.state.acronym +
-      ' | Vagas: ' +
-      vacancy.availablePositions
+      " | Vagas: " +
+      vacancy.availablePositions;
 
-    if (vacancy.pcdOnly) subtitle += ' | PCD'
+    if (vacancy.pcdOnly) subtitle += " | PCD";
 
-    const description = await this.getDescription(vacancy.id)
+    const description = await this.getDescription(vacancy.id);
 
-    const _id = await createHashId(vacancy.name, description)
+    const _id = await createHashId(vacancy.name, description);
     return {
       _id,
       title: vacancy.name,
@@ -93,42 +95,42 @@ export abstract class SolidesScraper implements Scraper {
       description,
       url: vacancy.linkVacancy,
       source: this.source,
-    }
+    };
   }
 
   private async getDescription(id: number) {
     const response = await fetch(
       `https://api.solides.jobs/v2/vacancy/${id}`,
-    ).then((res) => res.json())
+    ).then((res) => res.json());
 
-    if (!response?.data) return ''
+    if (!response?.data) return "";
 
-    const data = response.data as Vacancy
-    if (!data.description) return ''
+    const data = response.data as Vacancy;
+    if (!data.description) return "";
 
-    const $ = cheerio.load(data.description)
+    const $ = cheerio.load(data.description);
 
-    let description = ''
+    let description = "";
 
-    $('p, ul').each((_, element) => {
-      if (description != '') description += '\n\n'
+    $("p, ul").each((_, element) => {
+      if (description != "") description += "\n\n";
 
-      if (element.name == 'p') {
+      if (element.name == "p") {
         description += $(element)
-          .find('br')
-          .replaceWith('\n')
+          .find("br")
+          .replaceWith("\n")
           .end()
           .text()
-          .trim()
-      } else if (element.name == 'ul') {
+          .trim();
+      } else if (element.name == "ul") {
         $(element)
-          .children('li')
+          .children("li")
           .each((_, li) => {
-            description += ' - ' + $(li).text().trim() + '\n'
-          })
+            description += " - " + $(li).text().trim() + "\n";
+          });
       }
-    })
+    });
 
-    return description
+    return description;
   }
 }
