936dd9675e4b1eb2ad69129ac6baf3a00dd0b3c6
Hemillainy Santos <hemillainy.santos@ccc.ufcg.edu.br>
2023-02-09 22:09:43 -0300

    fix lint

--- scrapers/Codeby.ts
+++ scrapers/Codeby.ts
@@ -1,87 +1,87 @@
-import * as cheerio from 'https://esm.sh/cheerio@1.0.0-rc.12'
+import * as cheerio from "https://esm.sh/cheerio@1.0.0-rc.12";
 
-import { Opportunity } from '../architecture/Opportunity.ts'
-import { Scraper } from '../architecture/Scraper.ts'
-import { createHashId } from '../utils/utils.ts'
+import { Opportunity } from "../architecture/Opportunity.ts";
+import { Scraper } from "../architecture/Scraper.ts";
+import { createHashId } from "../utils/utils.ts";
 
 class CodebyScraper implements Scraper {
-  url = 'https://codeby.global/pages/vagas/'
+  url = "https://codeby.global/pages/vagas/";
 
   async execute() {
-    let opportunities: Opportunity[] = []
+    let opportunities: Opportunity[] = [];
 
-    const res = await fetch(this.url)
-    const html = await res.text()
+    const res = await fetch(this.url);
+    const html = await res.text();
 
-    const $ = cheerio.load(html)
+    const $ = cheerio.load(html);
 
-    const element = $('main > section > div > div.rte')
+    const element = $("main > section > div > div.rte");
 
-    let title = ''
-    let description = ''
+    let title = "";
+    let description = "";
 
     element.children().each((_, child) => {
-      if (child.tagName == 'h2') {
-        const text = $(child).text().trim()
-        if (text) title = text
-      } else if (child.tagName == 'hr') {
+      if (child.tagName == "h2") {
+        const text = $(child).text().trim();
+        if (text) title = text;
+      } else if (child.tagName == "hr") {
         opportunities.push({
-          _id: '',
+          _id: "",
           title,
           description,
           url: this.url,
           source: {
-            name: 'CodeBy',
+            name: "CodeBy",
             url: this.url,
           },
-        })
+        });
 
-        title = ''
-        description = ''
+        title = "";
+        description = "";
       } else {
-        if (child.tagName == 'div') {
-          const parsed = $(child)
-          const text = parsed.text().trim()
+        if (child.tagName == "div") {
+          const parsed = $(child);
+          const text = parsed.text().trim();
           if (text) {
-            description += '\n'
+            description += "\n";
 
-            if (parsed.children('strong').length == 1) {
-              description += '\n' + parsed.text().trim() + '\n'
+            if (parsed.children("strong").length == 1) {
+              description += "\n" + parsed.text().trim() + "\n";
             } else {
-              description += text
+              description += text;
             }
           }
-        } else if (child.tagName == 'ul') {
+        } else if (child.tagName == "ul") {
           $(child)
-            .children('li')
+            .children("li")
             .each((_, li) => {
-              description += '\n - ' + $(li).text().trim()
-            })
-        } else if (child.tagName == 'p') {
-          const parsed = $(child)
-          const text = parsed.text().trim()
-          if (text && text != '&nbsp;') {
-            description += '\n'
-            description += '\n' + text + '\n'
+              description += "\n - " + $(li).text().trim();
+            });
+        } else if (child.tagName == "p") {
+          const parsed = $(child);
+          const text = parsed.text().trim();
+          if (text && text != "&nbsp;") {
+            description += "\n";
+            description += "\n" + text + "\n";
           }
         }
       }
-    })
+    });
 
     opportunities = await Promise.all(
       opportunities.map(async (o: Opportunity) => {
-        let { _id, ...opportunityWithoutId } = o
+        let { _id, ...opportunityWithoutId } = o;
 
-        _id = await createHashId(o.title, o.description)
+        _id = await createHashId(o.title, o.description);
         return {
           _id,
           ...opportunityWithoutId,
-        }
+        };
       }),
-    )
+    );
 
-    return opportunities
+    return opportunities;
   }
 }
 
-export const Codeby = new CodebyScraper()
+export const Codeby = new CodebyScraper();
--- scrapers/M3.ts
+++ scrapers/M3.ts
@@ -1,100 +1,101 @@
-import * as cheerio from 'https://esm.sh/cheerio@1.0.0-rc.12'
+import * as cheerio from "https://esm.sh/cheerio@1.0.0-rc.12";
 
-import { Opportunity } from '../architecture/Opportunity.ts'
-import { Scraper } from '../architecture/Scraper.ts'
-import { createHashId } from '../utils/utils.ts'
+import { Opportunity } from "../architecture/Opportunity.ts";
+import { Scraper } from "../architecture/Scraper.ts";
+import { createHashId } from "../utils/utils.ts";
 
 class M3Scraper implements Scraper {
-  url = 'https://m3ecommerce.com/trabalhe-conosco/'
+  url = "https://m3ecommerce.com/trabalhe-conosco/";
 
   async execute() {
-    const opportunities: Opportunity[] = []
+    let opportunities: Opportunity[] = [];
 
-    const res = await fetch(this.url)
-    const html = await res.text()
+    const res = await fetch(this.url);
+    const html = await res.text();
 
-    const $ = cheerio.load(html)
+    const $ = cheerio.load(html);
 
-    const elements = $('.join-us-container__vagas__list')
+    const elements = $(".join-us-container__vagas__list");
 
     for (const element of elements) {
-      const parsed = $(element)
+      const parsed = $(element);
 
       const title = parsed
-        .children('.join-us-container__vagas__list__text')
-        .children('.join-us-container__vagas__list__text__title')
-        .children('h3')
-        .text()
+        .children(".join-us-container__vagas__list__text")
+        .children(".join-us-container__vagas__list__text__title")
+        .children("h3")
+        .text();
 
       const type = parsed
-        .children('.join-us-container__vagas__list__text')
-        .children('.join-us-container__vagas__list__text__content')
-        .children('span')
-        .text()
+        .children(".join-us-container__vagas__list__text")
+        .children(".join-us-container__vagas__list__text__content")
+        .children("span")
+        .text();
 
       const extra = parsed
-        .children('.join-us-container__vagas__list__text')
-        .children('.join-us-container__vagas__list__text__content')
-        .children('p')
-        .text()
+        .children(".join-us-container__vagas__list__text")
+        .children(".join-us-container__vagas__list__text__content")
+        .children("p")
+        .text();
 
-      const subtitle = type + ' | ' + extra
+      const subtitle = type + " | " + extra;
 
-      let description = ''
+      let description = "";
 
       parsed
-        .children('.join-us-container__vagas__list__content')
+        .children(".join-us-container__vagas__list__content")
         .children()
         .each((_, child) => {
-          if (description != '') description += '\n\n'
+          if (description != "") description += "\n\n";
 
-          if (child.tagName == 'p') description += $(child).text().trim()
-          else if (child.tagName == 'ul') {
+          if (child.tagName == "p") description += $(child).text().trim();
+          else if (child.tagName == "ul") {
             $(child)
-              .children('li')
+              .children("li")
               .each((_, li) => {
-                description += ' - ' + $(li).text().trim() + '\n'
-              })
-          } else if (child.tagName == 'div') {
-            description += $(child).children('span').text().trim()
+                description += " - " + $(li).text().trim() + "\n";
+              });
+          } else if (child.tagName == "div") {
+            description += $(child).children("span").text().trim();
             $(child)
-              .children('ul')
-              .children('li')
+              .children("ul")
+              .children("li")
               .each((_, li) => {
-                description += ' - ' + $(li).text().trim() + '\n'
-              })
+                description += " - " + $(li).text().trim() + "\n";
+              });
           }
-        })
+        });
 
       description +=
-        '\n\nLINK DO FORMULÁRIO: https://app.pipefy.com/public/form/cjJmu6-g'
+        "\n\nLINK DO FORMULÁRIO: https://app.pipefy.com/public/form/cjJmu6-g";
 
       opportunities.push({
+        _id: "",
         title: title,
         subtitle: subtitle,
         description: description,
         url: this.url,
         source: {
-          name: 'M3',
+          name: "M3",
           url: this.url,
         },
-      })
+      });
     }
 
     opportunities = await Promise.all(
       opportunities.map(async (o: Opportunity) => {
-        let { _id, ...opportunityWithoutId } = o
+        let { _id, ...opportunityWithoutId } = o;
 
-        _id = await createHashId(o.title, o.description)
+        _id = await createHashId(o.title, o.description);
         return {
           _id,
           ...opportunityWithoutId,
-        }
+        };
       }),
-    )
+    );
 
-    return opportunities
+    return opportunities;
   }
 }
 
-export const M3 = new M3Scraper()
+export const M3 = new M3Scraper();
