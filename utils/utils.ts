c9152946e463fecfffa2493cc45ba208e72b0b5b
Hemillainy Santos <hemillainy.santos@ccc.ufcg.edu.br>
2023-02-23 00:06:04 -0300

    fix lint

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,18 +1,18 @@
 import {
   crypto,
   toHashString,
-} from 'https://deno.land/std@0.176.0/crypto/mod.ts'
+} from "https://deno.land/std@0.176.0/crypto/mod.ts";
 
 export async function createHashId(
   title: string,
   subtitle: string,
   description: string,
 ) {
-  const textToEncode = `${title}${subtitle}${description}`
+  const textToEncode = `${title}${subtitle}${description}`;
   const hash = await crypto.subtle.digest(
-    'MD5',
+    "MD5",
     new TextEncoder().encode(textToEncode),
-  )
+  );
 
-  return toHashString(hash)
+  return toHashString(hash);
 }
